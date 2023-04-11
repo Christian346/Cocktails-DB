b573ccab5b5f8a61b68fcde88902917427335288
ChristianSanchez <chris.346@hotmail.com>
2023-04-11 14:04:59 -0400

    Updates

---
+++ precache-manifest.8598e4a1643d8f2a05bd6903a68ba53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d460f99e990b38b7c3dc315c4d6d8ac",
+    "url": "/Cocktails-DB/index.html"
+  },
+  {
+    "revision": "712374df5d0726778194",
+    "url": "/Cocktails-DB/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "15d247eb46d04347fb97",
+    "url": "/Cocktails-DB/static/js/2.2adbf3b7.chunk.js"
+  },
+  {
+    "revision": "3031e9cf5b1ff70737805e00c9aae204",
+    "url": "/Cocktails-DB/static/js/2.2adbf3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712374df5d0726778194",
+    "url": "/Cocktails-DB/static/js/main.1cfd5542.chunk.js"
+  },
+  {
+    "revision": "85c141ca2856f9b9f902",
+    "url": "/Cocktails-DB/static/js/runtime-main.d9906503.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/Cocktails-DB/static/media/logo.f3ad7664.svg"
+  }
+]);
