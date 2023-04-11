d10a82e810e9ac1aa44bd1513d12a1d439e3aa73
ChristianSanchez <chris.346@hotmail.com>
2023-04-11 15:42:28 -0400

    first commit 2.1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Error from './pages/Error';
 import Navbar from './components/Navbar';
 function App() {
   return (
-    <Router>
+    <Router basename='/Cocktails-DB'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
