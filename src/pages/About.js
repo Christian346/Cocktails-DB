d68c371a29b502527850e2d9c77154e28b4027af
ChristianSanchez <chris.346@hotmail.com>
2023-04-11 13:56:55 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+       In this project we can see how to fetch a url and how you can filter the specific data 'drink' objects.
+      </p>
+    </section>
+  );
+}
